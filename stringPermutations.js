2e08593730772fe417b889fd30146f57f3a21ecd
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-08 17:30:38 -0500

    Add initial recursive solution

--- stringPermutations.js
+++ stringPermutations.js
@@ -1,5 +1,22 @@
 let findAllPermutations = (string) => {
-  return string
+if (string.length < 2){
+    return string
+  }
+
+  let permutationsArray = [] 
+  
+  for (let i = 0; i < string.length; i++){
+    let char = string[i]
+
+    if (string.indexOf(char) != i)
+    continue
+
+    let remainder = string.slice(0, i) + string.slice(i + 1, string.length)
+
+    for (let permutation of findAllPermutations(remainder)){
+      permutationsArray.push(char + permutation) }
+  }
+  return permutationsArray
 }
 
 module.exports = findAllPermutations
